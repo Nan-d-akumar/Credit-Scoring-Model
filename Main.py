72bd7debfc18128d7fb011befef1a88e036889e6
Nandakumar <nandusangeeth@gmail.com>
2026-02-22 13:36:11 +0530

    added ROC curve and model performance table

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix,roc_curve, auc
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 import pickle
-
+import matplotlib.pyplot as plt
 
 # Load dataset
 df = pd.read_csv("german_credit_data.csv")
@@ -95,3 +95,49 @@ with open("credit_model.pkl", "wb") as file:
     pickle.dump(model, file)
 
 print("Model saved successfully!")
+
+# Logistic Regression Metrics
+log_accuracy = accuracy_score(y_test, y_pred)
+log_recall_0 = classification_report(y_test, y_pred, output_dict=True)['0']['recall']
+
+# Random Forest Metrics
+rf_accuracy = accuracy_score(y_test, rf_pred)
+rf_recall_0 = classification_report(y_test, rf_pred, output_dict=True)['0']['recall']
+
+# Create comparison table
+performance_table = pd.DataFrame({
+    "Model": ["Logistic Regression", "Random Forest"],
+    "Accuracy": [round(log_accuracy, 3), round(rf_accuracy, 3)],
+    "Recall (Bad Credit - 0)": [round(log_recall_0, 3), round(rf_recall_0, 3)]
+})
+
+print("\n==============================")
+print("MODEL PERFORMANCE COMPARISON")
+print("==============================")
+print(performance_table)
+
+
+# Get probability scores
+log_probs = model.predict_proba(X_test)[:, 1]
+rf_probs = rf_model.predict_proba(X_test)[:, 1]
+
+# Compute ROC values
+log_fpr, log_tpr, _ = roc_curve(y_test, log_probs)
+rf_fpr, rf_tpr, _ = roc_curve(y_test, rf_probs)
+
+# Compute AUC
+log_auc = auc(log_fpr, log_tpr)
+rf_auc = auc(rf_fpr, rf_tpr)
+
+# Plot ROC Curve
+plt.figure(figsize=(8,6))
+plt.plot(log_fpr, log_tpr, label=f'Logistic Regression (AUC = {log_auc:.2f})')
+plt.plot(rf_fpr, rf_tpr, label=f'Random Forest (AUC = {rf_auc:.2f})')
+plt.plot([0, 1], [0, 1], linestyle='--')
+
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("ROC Curve Comparison")
+plt.legend()
+plt.grid()
+plt.show()
